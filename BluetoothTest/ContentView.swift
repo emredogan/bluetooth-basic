700012ce466953b8776e02023b4febf378fbfdb8
Emre Dogan <emre.dogan@shortcut.no>
2023-09-18 12:14:56 +0200

    basic working version

--- BluetoothTest/ContentView.swift
+++ BluetoothTest/ContentView.swift
@@ -6,16 +6,43 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+class BluetoothViewModel: NSObject, ObservableObject {
+	private var centeralManager: CBCentralManager?
+	private var peripherals: [CBPeripheral] = []
+	@Published var peripheralNames: [String] = []
+	
+	override init() {
+		super.init()
+		self.centeralManager = CBCentralManager(delegate: self, queue: .main)
+	}
+}
+
+extension BluetoothViewModel: CBCentralManagerDelegate {
+	func centralManagerDidUpdateState(_ central: CBCentralManager) {
+		if central.state == .poweredOn {
+			self.centeralManager?.scanForPeripherals(withServices: nil)
+		}
+	}
+	
+	func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+		if !peripherals.contains(peripheral) {
+			self.peripherals.append(peripheral)
+			self.peripheralNames.append(peripheral.name ?? "Unnamed Device")
+		}
+	}
+}
 
 struct ContentView: View {
+	@ObservedObject private var viewModel: BluetoothViewModel = BluetoothViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationView {
+			List(viewModel.peripheralNames, id: \.self) { name in
+				Text(name)
+			}
+			.navigationTitle("Bluetooth List")
+		}
     }
 }
 
