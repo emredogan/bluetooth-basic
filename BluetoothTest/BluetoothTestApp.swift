2d8faf1910089669917a20381aec5907e74bbdc2
Emre Dogan <emre.dogan@shortcut.no>
2023-09-18 09:25:32 +0200

    Initial Commit

---
+++ BluetoothTest/BluetoothTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothTestApp.swift
+//  BluetoothTest
+//
+//  Created by Emre Dogan on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
